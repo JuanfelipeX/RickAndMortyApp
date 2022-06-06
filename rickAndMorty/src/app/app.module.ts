b7edbaf8c143d511ef9608b8d8a372f559c07277
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-06 15:31:09 -0500

    ng g c shared/components/header

--- rickAndMorty/src/app/app.module.ts
+++ rickAndMorty/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsearchComponent } from './shared/components/formsearch/formsearch.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormsearchComponent
+    FormsearchComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
