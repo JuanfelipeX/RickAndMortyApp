2c74f19dc9d7cc3f5a4ffaeb89d315f3da14f624
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-06 15:56:34 -0500

    creating method getDetails in character.service

--- rickAndMorty/src/app/shared/services/character.service.ts
+++ rickAndMorty/src/app/shared/services/character.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Character } from '../interfaces/character.interface';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharacterService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getDetails(id: number) {
+    return this.http
+      .get<Character>(`${environment.baseUrlAPI}/${id}`)
+      .pipe(catchError((err) => this.handleHttpError(err)));
+  }
+
+  handleHttpError(err: any): any {
+    throw new Error('Method not implemented.');
+  }
 }
