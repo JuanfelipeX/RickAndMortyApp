f4ddae6daca99b3949a0988ac65a9b392a897c7f
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-06 16:32:21 -0500

    importing and exporting ...myComponents

--- rickAndMorty/src/app/components/pages/characters/characters.module.ts
+++ rickAndMorty/src/app/components/pages/characters/characters.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppComponent } from '@app/app.component';
+import { HeaderComponent } from '@app/shared/components/header/header.component';
+import { CharacterListComponent } from '@characters/character-list/character-list.component';
+import { CharacterDetailsComponent } from '@characters/character-details/character-details.component';
+import { RouterModule } from '@angular/router';
 
+
+ const myComponents = [
+   CharacterDetailsComponent,
+   CharacterListComponent,
+   //CharacterComponent,
+ ];
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [...myComponents],
+  imports: [CommonModule, RouterModule], //InfiniteScrollModule],
+  exports: [...myComponents],
 })
-export class CharactersModule { }
+export class CharactersModule {}
