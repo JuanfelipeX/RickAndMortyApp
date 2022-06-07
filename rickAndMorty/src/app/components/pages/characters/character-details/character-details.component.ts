bdd5df16743c2944424876611d9a6335b3c350f0
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-07 12:06:05 -0500

    creating methos for visualizate the detail characters

--- rickAndMorty/src/app/components/pages/characters/character-details/character-details.component.ts
+++ rickAndMorty/src/app/components/pages/characters/character-details/character-details.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Observable } from 'rxjs';
+import { CharacterService } from '@shared/services/character.service';
+import { Character } from '@shared/interfaces/character.interface';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.component.html',
-  styleUrls: ['./character-details.component.scss']
+  styleUrls: ['./character-details.component.scss'],
 })
+  
 export class CharacterDetailsComponent implements OnInit {
+  character$!: Observable<Character>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private characterSvc: CharacterService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.pipe(take(1)).subscribe((params) => {
+      const id = params['id'];
+      //this.character$?.subscribe(x => this.characterSvc.getDetails(id)); 
+      this.character$ = this.characterSvc.getDetails(id);
+      
+    });
   }
 
+  onGoBack(): void {
+    this.location.back();
+  }
 }
